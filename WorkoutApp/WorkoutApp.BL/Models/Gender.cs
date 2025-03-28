4362489447717cedfdc2eeb1dac650ede3aaebd5
EBuravskiy <elric0sphinx@gmail.com>
2025-03-28 14:56:54 +0500

    Created models User, Gender

---
+++ WorkoutApp/WorkoutApp.BL/Models/Gender.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace WorkoutApp.BL.Models
+{
+    /// <summary>
+    /// Gender
+    /// </summary>
+    internal class Gender
+    {
+        /// <summary>
+        /// GenderID
+        /// </summary>
+        public int GenderID { get; }
+        /// <summary>
+        /// GenderName
+        /// </summary>
+        public string GenderName { get; }
+        /// <summary>
+        /// Create new Gender
+        /// </summary>
+        /// <param name="genderName">Gender name</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string genderName)
+        {
+            if (string.IsNullOrWhiteSpace(genderName))
+            {
+                throw new ArgumentNullException("Gender can't be null or empty", nameof(genderName));
+            }
+            genderName = genderName.Trim().ToLower();
+            //TextInfo myTI = new CultureInfo("ru-RU", false).TextInfo;
+            //myTI.ToTitleCase(name);
+            //Name = myTI.ToString();
+            GenderName = genderName;
+        }
+
+        public override string ToString()
+        {
+            return GenderName;
+        }
+    }
+}
---
+++ WorkoutApp/WorkoutApp.BL/Models/User.cs
@@ -0,0 +1,77 @@
+﻿namespace WorkoutApp.BL.Models
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    internal class User
+    {
+        #region User properties
+        /// <summary>
+        /// UserID
+        /// </summary>
+        public int UserID { get; }
+        /// <summary>
+        /// Name of user
+        /// </summary>
+        public string UserName { get; }
+        /// <summary>
+        /// User's gender
+        /// </summary>
+        public Gender Gender { get; }
+        /// <summary>
+        /// User's birth date
+        /// </summary>
+        public DateTime BirthDate { get; }
+        /// <summary>
+        /// User's weight
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// User's height
+        /// </summary>
+        public double Height { get; set; }
+        /// <summary>
+        /// User's email
+        /// </summary>
+        public string Email { get; set; }
+        #endregion
+        /// <summary>
+        /// Create new user
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="gender"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="weight"></param>
+        /// <param name="height"></param>
+        /// <param name="email"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public User(string userName, 
+            Gender gender, 
+            DateTime birthDate, 
+            double weight, 
+            double height, 
+            string email)
+        {
+            #region Check inputs
+            if (string.IsNullOrWhiteSpace(userName)) throw new ArgumentNullException("Name can't be null or empty", nameof(userName));
+            if(gender == null) throw new ArgumentNullException("Gender can't be null or empty", nameof(gender));
+            if(birthDate < DateTime.Parse("01.01.1900") && birthDate >= DateTime.Now) throw new ArgumentNullException("Imposible birth date", nameof(birthDate));
+            if(weight <= 0) throw new ArgumentNullException("Wrong weigth", nameof(weight));
+            if(height <= 0) throw new ArgumentNullException("Wrong heigth", nameof(height));
+            if (string.IsNullOrEmpty(email)) throw new ArgumentNullException("Email can't be null or empty", nameof(email));
+            //TODO: сделать проверку email с помощью регулярного выражения
+            #endregion
+            UserName = userName;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return UserName;
+        }
+    }
+}
